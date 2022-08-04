e539d58f6e10d9a9d83a78b24aa70c6d78540b38
José Luis Rodrigues Da Silva <joserodrigues@MacBook-Pro-de-Jose.local>
2022-08-04 10:59:04 -0400

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task')
+    .setDescription('The Task API description')
+    .setVersion('1.0')
+    .addTag('task')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
